dbccc817def6dbeda0f1b7746fdec36efcea7310
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-02-14 21:41:38 +0530

    BookMyShow js file

---
+++ Day-9/bookmyShow/bookMyShow.js
@@ -0,0 +1,18 @@
+const movies = [
+    {
+        title : "Pattaudu",
+        genre :["Action","Mystery","thiriller"]
+    },
+    {
+        title:"Padmaavat",
+        genre:["Drama","Period","Romantic"]
+    },
+    {
+        title:"Thandel",
+        genre:["Action","Drama","Romantic"]
+    },
+    {
+        title:"Sankranthiki Vasthunam",
+        genre:["Action","Comedy","Romantic"]
+    }
+];
