3dfdc60077a4591d4f2f0f37471a85029075d037
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-02-18 07:36:24 +0530

    Commit changes

--- Day-6/practice1.js
+++ Day-6/practice1.js
@@ -4,15 +4,14 @@
 let yourName = "Rajesh";
 let yourAge = 22 ;
 let isStudent = true;
-
-console.log(yourName +"\n"+ yourAge+ "\n" + isStudent);
-
-// same data using object 
-
 const graduationYear = 2026;
 
-//  graduationYear = 2025; // no it's not working
+console.log(yourName , yourAge, isStudent , graduationYear);
 
+console.log(typeof(yourName) , typeof(yourAge), typeof(isStudent),typeof(graduationYear));
+
+// same data using object 
+//  graduationYear = 2025; // no it's not working
   // we cannot ressigned here const variable
 
-  console.log(graduationYear);
+  
