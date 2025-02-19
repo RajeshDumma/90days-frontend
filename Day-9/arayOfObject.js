04c9e69acb6bb81d4344fb3dcbc89612a927fce6
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-02-19 15:06:50 +0530

    array of objects

---
+++ Day-9/arayOfObject.js
@@ -0,0 +1,36 @@
+
+// Define an array of product objects, where each object represents a product with 
+// properties: name, price, category, and stock.
+const products = [
+   // Object 1: 
+   { name: "Laptop", price: 4500, category: "Electronics", stock: 10 },
+   
+   // Object 2: 
+   { name: "Smartphone", price: 50000, category: "Electronics", stock: 20 },
+   
+   // Object 3: 
+   { name: "Running Shoes", price: 3000, category: "Footwear", stock: 15 },
+   
+   // Object 4:
+   { name: "Backpack", price: 2000, category: "Accessories", stock: 25 },
+   
+   // Object 5: 
+   { name: "Wireless Headphones", price: 4500, category: "Electronics", stock: 18 }
+];
+
+// Log the entire array of products to the console
+console.log(products);
+
+// Log the product at index 3 (fourth item in the array, which is "Backpack")
+console.log("Array [3]", products[3]);
+
+// Log the product at index 1 (second item in the array, which is "Smartphone")
+console.log("Array[1]", products[1]);
+
+// checking size of array
+
+console.log("Length of Array is : ",products.length);
+
+
+
+
